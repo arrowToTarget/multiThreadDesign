dbf5ba128a10f5a38a9cd15f37a66f627eecf4b5
arrowToTarget <1017635978@qq.com>
2016-03-23 20:14:12 +0800

    add TaskProcessorFactory and TaskProcessorManager

--- multiThread/src/com/lewis/multiThread/readWriteLock/pattern/Data.java
+++ multiThread/src/com/lewis/multiThread/readWriteLock/pattern/Data.java
@@ -1,10 +1,33 @@
 package com.lewis.multiThread.readWriteLock.pattern;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by zhangminghua on 2016/3/23.
  */
 public class Data {
 
+    public static void main(String[] args) {
+        List<Integer>  list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        List<Integer>  list1 = new LinkedList<Integer>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(6);
+        System.out.println(list);
+        System.out.println(list1);
+        System.out.println("==============");
+        list.removeAll(list1);
+        System.out.println(list);
+        System.out.println(list1);
+    }
 
     private class ReadWriteLock{
         private final GuardStrategy guardStrategy;
